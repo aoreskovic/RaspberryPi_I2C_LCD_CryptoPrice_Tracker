7e20d495cc9b3f369b1c38b25b2189281464f581
raspitajse <raspitajse@gmail.com>
2017-12-18 13:57:23 +0100

    First commit

---
+++ main.py
@@ -0,0 +1,42 @@
+import I2C_LCD_driver
+import json
+import requests
+from time import *
+
+
+sleep(5)
+response = requests.get("https://api.coinmarketcap.com/v1/ticker/?limit=15")
+
+mylcd = I2C_LCD_driver.lcd()
+
+parsedData = response.json()
+
+def getPrice(coin):
+    for name in parsedData:
+        if name["symbol"] == coin:
+            return (name["price_usd"]+"00")[0:7]
+    return 0
+
+def getChange(coin):
+    for name in parsedData:
+        if name["symbol"] == coin:
+            if name["percent_change_1h"][0] == "-":
+                return name["percent_change_1h"]
+            else:
+                return "+" + name["percent_change_1h"]
+    return 0
+
+
+
+mylcd.lcd_clear()
+mylcd.lcd_display_string("BTC " + getPrice("BTC") + " $ " + getChange("BTC") + "%", 1, 0)
+mylcd.lcd_display_string("ETH " + getPrice("ETH") + " $ " + getChange("ETH") + "%", 2, 0)
+mylcd.lcd_display_string("LTC " + getPrice("LTC") + " $ " + getChange("LTC") + "%", 3, 0)
+mylcd.lcd_display_string("ADA " + getPrice("ADA") + " $ " + getChange("ADA") + "%", 4, 0)
+#mylcd.lcd_display_string("XRP " + getPrice("XRP"), 4, 10)
+#mylcd.lcd_display_string("IOT " + getPrice("MIOTA"), 3, 0)
+    
+
+
+    
+    
